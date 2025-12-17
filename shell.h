226be903627722de6cd6f9e44cd2f26f29675822
Denis12306 <denis.gavaud@gmail.com>
2025-12-17 17:24:47 +0100

    UpdatingFiles

---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <errno.h>
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+int ls_function(void);
+
+
+
+
+#endif
