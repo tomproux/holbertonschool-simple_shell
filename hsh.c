a6b0d930f736977086d3332197e5f5bdc09c4388
Denis12306 <denis.gavaud@gmail.com>
2026-01-02 21:38:11 +0100

    UpdateFiles

---
+++ hsh.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+/**
+ * creation of the shell
+ * Return nothing
+ */
+int main(void)
+{
+    char *line = NULL;
+    char **argv = NULL;
+    int interactive = isatty(STDIN_FILENO);
+
+    while (1)
+    {
+    if (interactive)
+    display_prompt();
+
+    line = read_input();
+    if (line == NULL)
+    break;
+
+    argv = parse_line(line);
+    if (argv == NULL || argv[0] == NULL)
+    {
+    free(line);
+    free(argv);
+    continue;
+    }
+
+    if(handle_builtins(argv))
+    {
+        free(line);
+        free(argv);
+        continue;
+    }
+    execute_command(argv);
+    free(line);
+    free(argv);
+    }
+}
