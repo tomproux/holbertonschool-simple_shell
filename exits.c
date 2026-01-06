9b1aac5dc41657d5a50fff2cc699ef9c43ad26dc
Gavaud Denis <denis.gavaud@gmail.com>
2026-01-06 11:34:44 +0100

    Remove atoi function and update return statement
    Removed the atoi function implementation and adjusted return statement.

--- exits.c
+++ exits.c
@@ -11,35 +11,5 @@ int sortie(void)
 	{
 		exit(EXIT_SUCCESS);
 	}
-	return(0);
-}
-
-/**
- * atoi - a function that convert a string to an integer
- * @i : input integer
- * @sign : sign of the integer
- * @result : result integer
- * Return: integer value
- */
-int atoi(const char *str)
-{
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-	i++;
-
-	if(str[i] == '+' || str[i] == '-')
-	{
-		if(str[i] == '-')
-		sign = -1;
-		i++;
-	}
-	while(str[i] >= 0 && str[i] <= 9)
-	{
-	result = result * 10 + (str[i] - '0');
-	i++;
-	}
-	return(result * sign);
+	return (0);
 }
