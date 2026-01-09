432ef6b7c5aa840f912d82eac59cab36598e33c5
tomproux <tomproux53@gmail.com>
2026-01-09 09:34:21 +0000

    fonction execute

--- execute.c
+++ execute.c
@@ -1,42 +1,41 @@
 #include "shell.h"
 /**
  * execute_command - a function that execute an external command
- * @pid_t: is the parent identification
- * @cmd_path: is the path command  
- * Return nothing
+ * @argv: argument table
+ * Return: 1 is SUCCESS
  */
 int execute_command(char **argv)
 {
-    pid_t pid;
-    int status;
-    char *cmd_path;
+	pid_t pid;
+	int status;
+	char *cmd_path;
 
-    if (!argv || !argv[0])
-        return 0;
+	if (!argv || !argv[0])
+		return (0);
 
-    if (handle_builtins(argv))
-        return 0;
+	if (handle_builtins(argv))
+		return (0);
 
-    cmd_path = find_command_path(argv[0]);
-    if (!cmd_path)
-    {
-        fprintf(stderr, "%s: command not found\n", argv[0]);
-        return 127;
-    }
+	cmd_path = find_command_path(argv[0]);
+	if (!cmd_path)
+	{
+		fprintf(stderr, "%s: command not found\n", argv[0]);
+		return (127);
+	}
 
-    pid = fork();
-    if (pid == 0)
-    {
-        execve(cmd_path, argv, environ);
-        perror("execve");
-        exit(EXIT_FAILURE);
-    }
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(cmd_path, argv, environ);
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
 
-    waitpid(pid, &status, 0);
-    free(cmd_path);
+		waitpid(pid, &status, 0);
+		free(cmd_path);
 
-    if (WIFEXITED(status))
-        return WEXITSTATUS(status);
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
 
-    return 1;
+	return (1);
 }
