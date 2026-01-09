65ee8f0da8f7d555f09f842e9df74873e6f7f137
tomproux <tomproux53@gmail.com>
2026-01-09 14:35:46 +0000

    fonction error

--- errors.c
+++ errors.c
@@ -9,31 +9,30 @@
  */
 void print_error(char *hsh, int line, char *cmd, char *msg)
 {
-    char num[20];
-    int i = 0, n = line;
+	char num[20];
+	int i = 0, n = line;
 
-    write(STDERR_FILENO, hsh, _strlen(hsh));
-    write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, hsh, _strlen(hsh));
+	write(STDERR_FILENO, ": ", 2);
 
-    if (line >= 0)
-    {
-        if (n == 0)
-            num[i++] = '0';
+	if (line >= 0)
+	{
+		if (n == 0)
+			num[i++] = '0';
 
-        while (n)
-        {
-            num[i++] = (n % 10) + '0';
-            n /= 10;
-        }
+		while (n)
+		{
+			num[i++] = (n % 10) + '0';
+			n /= 10;
+		}
 
-        while (--i >= 0)
-            write(STDERR_FILENO, &num[i], 1);
+		while (--i >= 0)
+			write(STDERR_FILENO, &num[i], 1);
 
-        write(STDERR_FILENO, ": ", 2);
-    }
-
-    write(STDERR_FILENO, cmd, _strlen(cmd));
-    write(STDERR_FILENO, ": ", 2);
-    write(STDERR_FILENO, msg, _strlen(msg));
-    write(STDERR_FILENO, "\n", 1);
+		write(STDERR_FILENO, ": ", 2);
+	}
+	write(STDERR_FILENO, cmd, _strlen(cmd));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, msg, _strlen(msg));
+	write(STDERR_FILENO, "\n", 1);
 }
